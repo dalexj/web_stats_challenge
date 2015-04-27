e2571d7bc63e62dc15dbb5937899bd2a0234078a
Alex <96dalex@gmail.com>
2015-04-27 15:45:02 -0600

    add validations to he visit model + md5 hashing

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,3 +1,27 @@
 class Visit < Sequel::Model
-  
+  plugin :validation_helpers
+  def validate
+    super
+    validates_presence [:url, :created_at]
+  end
+
+  def after_create
+    create_hash
+    save
+    super
+  end
+
+  def digested_hash
+    @values[:hash]
+  end
+
+  private
+
+  def create_hash
+    self.hash = Digest::MD5.hexdigest(columns_to_digest)
+  end
+
+  def columns_to_digest
+    {id: id, url: url, referrer: referrer, created_at: created_at }.to_s
+  end
 end
